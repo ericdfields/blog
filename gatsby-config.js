ba64aba41639e895e2df1ff5f5ee97e0cf3b1554
Eric Brookfield <eric@ericbrookfield.com>
2019-01-25 10:16:29 -0500

    update header

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby + Netlify CMS Starter",
-    description:
-      "This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution."
+    title:
+      "Eric Brookfield | Web Developer, UX/Front-end focused | Boston, MA & DC",
+    description: 'Weblog or "blog".'
   },
   plugins: [
     "gatsby-plugin-react-helmet",
